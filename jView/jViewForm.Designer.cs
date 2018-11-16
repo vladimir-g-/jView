e3d12984aee5a6f33ca74c24c4085f43ce1a24ab
Vladimir Geraskin <phantom_vm@hotmail.com>
2018-11-17 01:32:33 +0300

    m

--- jView/jViewForm.Designer.cs
+++ jView/jViewForm.Designer.cs
@@ -122,7 +122,7 @@
             this.tabPage1.Controls.Add(this.jNodesTree);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(497, 221);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Tree";
@@ -157,7 +157,7 @@
             this.tabPage2.Controls.Add(this.originalFileText);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(497, 221);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Text";
@@ -170,7 +170,8 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.originalFileText.Location = new System.Drawing.Point(0, 3);
-            this.originalFileText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.originalFileText.Margin = new System.Windows.Forms.Padding(2);
+            this.originalFileText.MaxLength = 65000;
             this.originalFileText.Multiline = true;
             this.originalFileText.Name = "originalFileText";
             this.originalFileText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
