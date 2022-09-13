698c96ed7230f7e3d04d0f09175783a7d197c777
VladimirG <phant@MSI>
2022-09-13 22:42:05 +0500

    Added Setup Project. Changed .NET runtime version.

--- jView/Properties/AssemblyInfo.cs
+++ jView/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("jView")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("JSON Viewer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vladimir Geraskin")]
 [assembly: AssemblyProduct("jView")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
